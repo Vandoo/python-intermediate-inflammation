23f1b211fdcaa9d3c1d23423e49d3283b153f9ac
Kuda Nguruwe <vg9053@M0200-Mac.local>
2023-04-25 16:25:09 +0100

    convert tabs to spaces in the class_library.py issue number #9

--- models/class_library.py
+++ models/class_library.py
@@ -4,42 +4,42 @@ class Book:
         self.author = author 
 
     def __str__(self):
-    	return self.name + ' by ' + self.author
+        return self.name + ' by ' + self.author
 
 class Library(Book):
-	def __init__(self, name=None):
-		self.name = name
-		self.collections = []
-	
-	def add_book(self, bookname, bookauthor):
-		newbook = Book(bookname, bookauthor)
-		self.collections.append(newbook)
-		#self.collections.append(Book(bookname, bookauthor))
-	
-	def __str__(self):
-		return self.name
-	
-	def __len__(self):
-		return len(self.collections)
+    def __init__(self, name=None):
+        self.name = name
+        self.collections = []
+    
+    def add_book(self, bookname, bookauthor):
+        newbook = Book(bookname, bookauthor)
+        self.collections.append(newbook)
+        #self.collections.append(Book(bookname, bookauthor))
+    
+    def __str__(self):
+        return self.name
+    
+    def __len__(self):
+        return len(self.collections)
 
-	def __getitem__(self, key):
-		return self.collections[key]
-	
-	def by_author(self, author):
-		ans = []
-		for book in self.collections:
-			if author == book.author:
-				ans.append(book)
+    def __getitem__(self, key):
+        return self.collections[key]
+    
+    def by_author(self, author):
+        ans = []
+        for book in self.collections:
+            if author == book.author:
+                ans.append(book)
 
-		#if len(ans) > 0:
-		#	return ans
-		#else:
-		#	print('author does not exist!')
+        #if len(ans) > 0:
+        #	return ans
+        #else:
+        #	print('author does not exist!')
 
-		if not ans:
-			raise KeyError('Author does not exist')
+        if not ans:
+            raise KeyError('Author does not exist')
 
-		return ans
+        return ans
 
 
 library = Library()
@@ -59,4 +59,4 @@ for book in books:
     print(book)
 
 #books = library.by_author('Carol')
-	
+    
